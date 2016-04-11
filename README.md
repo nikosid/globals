c5eb7577d40507fd57bcaf21d3c5cbdb11e4f6b4
sid <nikosid@gmail.com>
2016-04-11 15:54:21 +0200

    firts commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# globals
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+composer require nikosid/globals
+```
+
+or add
+
+```
+"nikosid/globals": "dev-master"
+```
+
+to the require section of your `composer.json` file.
---
+++ src/globals.php
@@ -0,0 +1,29 @@
+<?php
+
+$is_cli = false;
+
+if (PHP_SAPI == 'cli') {
+    $is_cli = true;
+}
+
+function d($var)
+{
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+}
+
+function dd($var)
+{
+    global $is_cli;
+    echo $is_cli ? '' : '<pre>';
+    var_dump($var);
+    echo $is_cli ? '' : '</pre>';
+    die;
+}
+
+function ed($var)
+{
+    echo $var;
+    die;
+}
